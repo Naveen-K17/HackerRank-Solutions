431c88bb224dbc65917a7b71c75bbd28dda55f05
Naveen Siddappa Kattimani <naveenkattimani326@gmail.com>
2026-07-29 20:29:28 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 22447-python-if-else-Hackerrank/22447-python-if-else.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if n % 2 == 1: 
+        print("Weird")
+    else:
+        if n>=2 and n<=5 :
+            print("Not Weird")
+        elif n>=6 and n<=20:
+            print("Weird")
+        elif n>=20:
+            print("Not Weird")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
